001ab6426cb0ed12cec63bc8f4ded0329578120b
Shantanu Raj <s@sraj.me>
2017-05-14 16:01:08 +0530

    Copy info implementation from flutter gallery's grid list demo from

--- lib/widgets/feed_info.dart
+++ lib/widgets/feed_info.dart
@@ -4,6 +4,8 @@ import 'package:podcst_app/data/api.dart';
 import 'package:podcst_app/data/feed.dart';
 import 'package:podcst_app/data/podcst.dart';
 
+const double _kMinFlingVelocity = 800.0;
+
 class FeedInfo extends StatefulWidget {
   FeedInfo({Key key, this.podcst}) : super(key: key);
 
@@ -13,11 +15,18 @@ class FeedInfo extends StatefulWidget {
   _FeedInfoState createState() => new _FeedInfoState(podcst);
 }
 
-class _FeedInfoState extends State<FeedInfo> {
+class _FeedInfoState extends State<FeedInfo>
+    with SingleTickerProviderStateMixin {
   final Podcst _podcst;
 
   bool _isLoading;
   Feed _feed;
+  AnimationController _controller;
+  Animation<Offset> _flingAnimation;
+  Offset _offset = Offset.zero;
+  double _scale = 1.0;
+  Offset _normalizedOffset;
+  double _previousScale;
 
   _FeedInfoState(this._podcst);
 
@@ -25,7 +34,57 @@ class _FeedInfoState extends State<FeedInfo> {
   void initState() {
     super.initState();
     _isLoading = true;
+    _controller = new AnimationController(vsync: this)
+      ..addListener(_handleFlingAnimation);
     PodcstApi.getFeed(_podcst.feed).then(onFeedLoaded);
+  }
+
+  // The maximum offset value is 0,0. If the size of this renderer's box is w,h
+  // then the minimum offset value is w - _scale * w, h - _scale * h.
+  Offset _clampOffset(Offset offset) {
+    final Size size = context.size;
+    final Offset minOffset = new Offset(size.width, size.height) *
+        (1.0 - _scale);
+    return new Offset(offset.dx.clamp(minOffset.dx, 0.0).toDouble(),
+        offset.dy.clamp(minOffset.dy, 0.0).toDouble());
+  }
+
+  void _handleFlingAnimation() {
+    setState(() {
+      _offset = _flingAnimation.value;
+    });
+  }
+
+  void _handleOnScaleStart(ScaleStartDetails details) {
+    setState(() {
+      _previousScale = _scale;
+      _normalizedOffset = (details.focalPoint - _offset) / _scale;
+      // The fling animation stops if an input gesture starts.
+      _controller.stop();
+    });
+  }
+
+  void _handleOnScaleUpdate(ScaleUpdateDetails details) {
+    setState(() {
+      _scale = (_previousScale * details.scale).clamp(1.0, 4.0).toDouble();
+      // Ensure that image location under the focal point stays in the same place despite scaling.
+      _offset = _clampOffset(details.focalPoint - _normalizedOffset * _scale);
+    });
+  }
+
+  void _handleOnScaleEnd(ScaleEndDetails details) {
+    final double magnitude = details.velocity.pixelsPerSecond.distance;
+    if (magnitude < _kMinFlingVelocity)
+      return;
+    final Offset direction = details.velocity.pixelsPerSecond / magnitude;
+    final double distance = (Offset.zero & context.size).shortestSide;
+    _flingAnimation = new Tween<Offset>(
+        begin: _offset,
+        end: _clampOffset(_offset + direction * distance)
+    ).animate(_controller);
+    _controller
+      ..value = 0.0
+      ..fling(velocity: magnitude / 1000.0);
   }
 
   void onFeedLoaded(Feed feed) {
@@ -37,20 +96,18 @@ class _FeedInfoState extends State<FeedInfo> {
 
   @override
   Widget build(BuildContext context) {
-    return new Row(
-      children: [
-        _isLoading ?
-        new Image.network(_podcst.cover, fit: BoxFit.cover) :
-        new Row(
-          children: [
-            new Image.network(
-              _feed.cover,
-              fit: BoxFit.cover,
-              width: 410.0,
-            ),
-          ],
+    return new GestureDetector(
+      onScaleStart: _handleOnScaleStart,
+      onScaleUpdate: _handleOnScaleUpdate,
+      onScaleEnd: _handleOnScaleEnd,
+      child: new ClipRect(
+        child: new Transform(
+            transform: new Matrix4.identity()
+              ..translate(_offset.dx, _offset.dy)
+              ..scale(_scale),
+            child: new Image.network(_podcst.cover, fit: BoxFit.cover),
         ),
-      ],
+      ),
     );
   }
 }
