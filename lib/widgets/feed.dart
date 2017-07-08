556ba971152599b82002e234741eca6bd6428054
Shantanu Raj <s@sraj.me>
2017-07-09 01:42:47 +0530

    Add shadow

--- lib/widgets/feed.dart
+++ lib/widgets/feed.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:podcst_app/data/feed.dart';
 import 'package:podcst_app/data/episode.dart';
 
+const Color _kKeyUmbraOpacity = const Color(0x33000000); // alpha = 0.2
+const Color _kKeyPenumbraOpacity = const Color(0x24000000); // alpha = 0.14
+const Color _kAmbientShadowOpacity = const Color(0x1F000000); // alpha = 0.12
+
 class FeedWidget extends StatelessWidget {
   final Feed _feed;
 
@@ -11,7 +15,12 @@ class FeedWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return new Container(
       decoration: new BoxDecoration(
-        color: Colors.black45,
+        color: Colors.black54,
+        boxShadow: const <BoxShadow>[
+          const BoxShadow(offset: const Offset(0.0, 3.0), blurRadius: 1.0, spreadRadius: -2.0, color: _kKeyUmbraOpacity),
+          const BoxShadow(offset: const Offset(0.0, 2.0), blurRadius: 2.0, spreadRadius: 0.0, color: _kKeyPenumbraOpacity),
+          const BoxShadow(offset: const Offset(0.0, 1.0), blurRadius: 5.0, spreadRadius: 0.0, color: _kAmbientShadowOpacity),
+        ],
       ),
       child: new Column(
         children: <Widget>[
@@ -43,7 +52,6 @@ class FeedItemWidget extends StatelessWidget {
       child: new ListTile(
         title: new Text(
           _episode.title,
-          style: new TextStyle(fontWeight: FontWeight.bold),
         ),
       ),
     );
